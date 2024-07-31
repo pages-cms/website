58f4ce1f34934968a0e79e1344ff26e646f63ce8
Ronan Berder <hunvreus@gmail.com>
2024-07-31 14:41:24 +0800

    Updating theme to match Next.js version

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,28 +2,58 @@
 module.exports = {
   content: ["./src/**/*.{njk,html,md,js}"],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
-        neutral: {
-          150: '#EDEDED',
-          750: '#333333',
-          850: '#1F1F1F',
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
         },
       },
-      height: {
-        screen: ['100vh /* fallback for Opera, IE and etc. */', '100dvh'],
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
-      zIndex: {
-        '100': '100',
-        '150': '150',
-        '200': '200',
-        '250': '250',
-        '300': '300',
-      }
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms')
-  ],
-  darkMode: 'class',
+  darkMode: ["class"],
 }
